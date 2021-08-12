00dd519769463853d75f172816442c520ab0aba8
Alexandra McCoy <alexandram@alexandram-a01.vmware.com>
2021-08-12 15:08:26 -0400

    Updated cli information in root

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "olympicctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI tool to query 2021 Tokyo Olympic Results",
+	Long: `Olympicctl is a CLI tool where its users are able to query results from the 2021 Olympic Games in Tokyo.
+The tool will return athlete and event information`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
